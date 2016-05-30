a908566a2a4d9cd39aca55538b5793c0c35058a5
Xemle <xemle@phtagr.org>
2016-05-30 21:16:12 +0200

    Skip points with max iterations

--- fractal/tile.go
+++ fractal/tile.go
@@ -4,47 +4,91 @@ import "fmt"
 
 // Tile calculates a part of fractal
 type Tile struct {
-	TopLeft, BottomRight *Complex
-	Width, Height        int
-	Points               []*Point
+  TopLeft, BottomRight *Complex
+  Width, Height        int
+  Points               []*Point
 }
 
 // NewTile create new tile
 func NewTile(tl, br *Complex, w, h int) *Tile {
-	var len = w * h
-	var points = make([]*Point, len, len)
-	dx := (br.Real - tl.Real) / float64(w)
-	dy := (br.Imaginary - tl.Imaginary) / float64(h)
-	var index int
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			r := tl.Real + float64(x)*dx
-			i := tl.Imaginary + float64(y)*dy
-			z := NewComplex()
-			c := NewComplex64(r, i)
-			points[index] = NewPoint().Set(z, c)
-			index++
-		}
-	}
-	return &Tile{tl, br, w, h, points}
+  var len = w * h
+  var points = make([]*Point, len, len)
+  return &Tile{tl, br, w, h, points}
+}
+
+func (t *Tile) getIndex(x, y int) int {
+  return y * t.Width + x
+}
+
+func (t *Tile) getPoint(x, y int) *Point {
+  index := t.getIndex(x, y)
+  if nil == t.Points[index] {
+    tl := t.TopLeft
+    br := t.BottomRight
+    dx := (br.Real - tl.Real) / float64(t.Width)
+    dy := (br.Imaginary - tl.Imaginary) / float64(t.Height)
+    r := tl.Real + float64(x)*dx
+    i := tl.Imaginary + float64(y)*dy
+    z := NewComplex()
+    c := NewComplex64(r, i)
+    t.Points[index] = NewPoint().Set(z, c)
+  }
+  return t.Points[index]
 }
 
 // Calculate calculates the points
 func (t *Tile) Calculate(max uint64) *Tile {
-	for _, p := range t.Points {
-		p.Calculate(max)
-	}
-
-	return t
+  changed := true
+  for changed {
+    changed = false
+    for y := 0; y < t.Height; y++ {
+      for x := 0; x < t.Width; x++ {
+        if t.skip(x, y, max) {
+          continue
+        }
+        t.getPoint(x, y).Calculate(max)
+        changed = true
+      }
+    }
+  }
+  return t
 }
 
+func (t *Tile) skip(x, y int, max uint64) bool {
+  index := t.getIndex(x, y)
+  if t.Points[index] != nil {
+    return true
+  }
+  if x == 0 || x == t.Width - 1 || y == 0 || y == t.Height - 1 {
+    return false
+  }
+  // Check if every neighbor has max iteration
+  for i := -1; i < 2; i++ {
+    for j := -1; j < 2; j++ {
+      if i != 0 && j != 0 && !t.hasMaxItertion(x + i, y + j, max) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+func (t *Tile) hasMaxItertion(x, y int, max uint64) bool {
+  index := t.getIndex(x, y)
+  p := t.Points[index]
+  return nil == p || p.Iterations == max
+}
 // Print prints the tile
 func (t *Tile) Print() {
-	for index, p := range t.Points {
-		fmt.Printf("%2d", p.Iterations)
-		fmt.Print(" ")
-		if index%t.Width == t.Width-1 {
-			fmt.Println("")
-		}
-	}
+  for index, p := range t.Points {
+    if p != nil {
+      fmt.Printf("%2d", p.Iterations)
+    } else {
+      fmt.Printf("xx")
+    }
+    fmt.Print(" ")
+    if index%t.Width == t.Width-1 {
+      fmt.Println("")
+    }
+  }
 }
--- server/tile_command.go
+++ server/tile_command.go
@@ -1,41 +1,45 @@
 package server
 
 import (
-	"fmt"
-	"mandelbrot/fractal"
+  "fmt"
+  "mandelbrot/fractal"
 )
 
 // TileCommand holds tile information to render
 type TileCommand struct {
-	Top          float64 `json:"top"`
-	Left         float64 `json:"left"`
-	Bottom       float64 `json:"bottom"`
-	Right        float64 `json:"right"`
-	Width        int     `json:"width"`
-	Height       int     `json:"height"`
-	MaxIteration uint64  `json:"maxIteration"`
+  Top          float64 `json:"top"`
+  Left         float64 `json:"left"`
+  Bottom       float64 `json:"bottom"`
+  Right        float64 `json:"right"`
+  Width        int     `json:"width"`
+  Height       int     `json:"height"`
+  MaxIteration uint64  `json:"maxIteration"`
 }
 
 // TileCommandResult holds the result
 type TileCommandResult struct {
-	TileCommand
-	Iterations []uint64 `json:"iterations"`
+  TileCommand
+  Iterations []uint64 `json:"iterations"`
 }
 
 // Calculate calculate tile
 func (t *TileCommand) Calculate() *TileCommandResult {
-	tl := fractal.NewComplex64(t.Left, t.Top)
-	br := fractal.NewComplex64(t.Right, t.Bottom)
+  tl := fractal.NewComplex64(t.Left, t.Top)
+  br := fractal.NewComplex64(t.Right, t.Bottom)
 
-	fmt.Println(tl)
-	fmt.Println(br)
-	tile := fractal.NewTile(tl, br, t.Width, t.Height)
-	tile.Calculate(t.MaxIteration)
+  fmt.Println(tl)
+  fmt.Println(br)
+  tile := fractal.NewTile(tl, br, t.Width, t.Height)
+  tile.Calculate(t.MaxIteration)
 
-	var iterations = make([]uint64, len(tile.Points), len(tile.Points))
-	for index, p := range tile.Points {
-		iterations[index] = p.Iterations
-	}
-	//tile.Print()
-	return &TileCommandResult{TileCommand: *t, Iterations: iterations}
+  var iterations = make([]uint64, len(tile.Points), len(tile.Points))
+  for index, p := range tile.Points {
+    if p != nil {
+      iterations[index] = p.Iterations
+    } else {
+      iterations[index] = t.MaxIteration
+    }
+  }
+  //tile.Print()
+  return &TileCommandResult{TileCommand: *t, Iterations: iterations}
 }
